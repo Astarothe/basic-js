b2255835ecd63bba51915618a226e88af025023b
Mishel Savich <minatosavich@gmail.com>
2020-10-30 01:30:23 +0300

    feat: add function count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(e) {
+
+  let array = 0;
+  for(let i = 0; i < e.length; i++){
+    console.log(e[i].length)
+    if(e[i].length > i){
+      for(let j = 0; j < e[i].length;j++){
+        if(e[i][j] === "^^"){
+          array+=1;
+        }
+
+      }
+    }
+  }
+
+
+  console.log(array);
+  return array;
 };
