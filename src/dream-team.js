3576b953ea9647e58c497f83a082bf94bf393180
Mishel Savich <minatosavich@gmail.com>
2020-10-30 03:46:59 +0300

    feat: add function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(e) {
+  let massive = [];
+  if(typeof(e) !== "object" || e === null){
+    return false
+  }
+  for(let i = 0; i < e.length; i++){
+    if((typeof(e[i])) !== "string"){
+      continue;
+    }
+    for(let j = 0; j <e[i].length;j++){
+      console.log(e[i][j]);
+      if(e[i][j] !== " "){
+       let perem = e[i][j].toUpperCase();
+        massive.push(perem);
+        console.log(massive);
+        break;
+      }
+    }
+  }
+  if(massive.length === 0){
+    return false;
+  }
+
+  console.log(massive);
+  let finaly = massive.sort().join("");
+  console.log(finaly);
+  return finaly;
 };
