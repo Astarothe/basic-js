3e3d5e03d2f2e139b31aec695284e4109676c3fd
Mishel Savich <minatosavich@gmail.com>
2020-10-31 20:22:29 +0300

    feat: add function transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let massive = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === "--discard-prev"){
+      massive.pop();
+  } else if(arr[i] === "--double-prev"){
+      if(arr[i - 1] === undefined){
+      }else{
+        massive.push(arr[i - 1]);
+      }
+  } else if(arr[i] === "--double-next"){
+      console.log(arr[i+1]);
+      if(arr[i + 1] === undefined){
+      }else{
+        massive.push(arr[i+1])
+      }
+    }else if(arr[i] === "--discard-next"){
+      if(arr[i + 1] === undefined){
+
+      }else{
+        if(arr[i + 2] === "--discard-prev" || arr[i + 2] === '--double-prev'){
+          i+=2
+          console.log("suka")
+        }else{
+        console.log("blyadd")
+          i++;
+        }
+      }
+    } else{
+      massive.push(arr[i])
+    }
+  }
+  console.log(massive);
+  return massive;
 };
