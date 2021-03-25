eacf5581cc6a46e226652b3234011fe7813e2571
Astarothe <minatosavich@gmail.com>
2021-03-25 17:56:07 +0300

    feat: end task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,21 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(e) {
-
-  let array = 0;
-  for(let i = 0; i < e.length; i++){
-    console.log(e[i].length)
-    if(e[i].length > i){
-      for(let j = 0; j < e[i].length;j++){
-        if(e[i][j] === "^^"){
-          array+=1;
-        }
-
+module.exports = function countCats(matrix) {
+  let sum = 0;
+  for(let i = 0; i < matrix.length; i++){
+    for(let j = 0; j < matrix[i].length; j++){
+    if(matrix[i][j] === "^^"){
+        sum++;
       }
     }
   }
-
-
-  console.log(array);
-  return array;
+  return sum;
 };
